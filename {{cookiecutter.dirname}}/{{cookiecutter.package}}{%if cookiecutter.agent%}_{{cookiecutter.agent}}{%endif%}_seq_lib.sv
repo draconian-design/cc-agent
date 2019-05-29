2dfeaf17ed754b8eb201375a5e2a66339325fa18
William Stucker <william.stucker@lightelligence.ai>
2019-05-29 19:26:19 -0400

    seq_lib fixes

--- {{cookiecutter.dirname}}/{{cookiecutter.package}}{%if cookiecutter.agent%}_{{cookiecutter.agent}}{%endif%}_seq_lib.sv
+++ {{cookiecutter.dirname}}/{{cookiecutter.package}}{%if cookiecutter.agent%}_{{cookiecutter.agent}}{%endif%}_seq_lib.sv
@@ -1,7 +1,7 @@
 {% include 'copyright' %}
   {%- set SQR = 'sqr' -%}
   {%- set SEQ_ITEM = 'item' %}
-  {%- set BASE_SEQ = 'seq_c' %}
+  {%- set BASE_SEQ = 'seq' %}
 {%- if cookiecutter.agent -%}
   {%- set SQR = cookiecutter.agent + '_sqr' -%}
   {%- set SEQ_ITEM = cookiecutter.agent + '_item' %}
@@ -15,13 +15,13 @@ typedef class {{ SQR }}_c;
 
 class {{ BASE_SEQ }}_c extends  uvm_sequence #({{ SEQ_ITEM }}_c);
 
-   `uvm_component_utils_begin({{ cookiecutter.package }}_pkg::{{ BASE_SEQ }}_c)
-   `uvm_component_utils_end
+   `uvm_object_utils_begin({{ cookiecutter.package }}_pkg::{{ BASE_SEQ }}_c)
+   `uvm_object_utils_end
 
    // `uvm_declare_p_sequencer({{ SQR }}_c)
 
-   function new(string name="{{ BASE_SEQ }}", uvm_component parent);
-      super.new(name, parent);
+   function new(string name="{{ BASE_SEQ }}");
+      super.new(name);
    endfunction : new
 
    virtual task body();
